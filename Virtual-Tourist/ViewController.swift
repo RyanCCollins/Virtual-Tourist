cf6e40d5d9bfb266ac2a0c61a45a08d9b7aa20c0
Tech Rapport <info@techrapport.com>
2015-11-20 20:20:28 -0500

    Initial Commit

---
+++ Virtual-Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual-Tourist
+//
+//  Created by Ryan Collins on 11/20/15.
+//  Copyright © 2015 Tech Rapport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
